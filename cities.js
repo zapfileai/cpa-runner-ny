535964d5ce3b1613b111e0b8e06f764cd800f4ad
Sreehari Annam <sannam@wescodist.com>
2026-07-07 12:08:58 -0400

    batch 11: next cities

--- cities.js
+++ cities.js
@@ -1,20 +1,8 @@
 const newYorkCities = [
-  "Stony Brook", "Stony Point", "Stormville", "Stuyvesant", "Suffern",
-  "Sugar Loaf", "Sunnyside", "Sylvan Beach", "Syosset", "Syracuse",
-  "Tannersville", "Tappan", "Tarrytown", "Thiells", "Thornwood",
-  "Tillson", "Tomkins Cove", "Tonawanda", "Treadwell", "Troy",
-  "Trumansburg", "Tuckahoe", "Tully", "Tupper Lake", "Tuxedo Park",
-  "Ulster Park", "Uniondale", "Upton", "Utica", "Valatie",
-  "Valhalla", "Valley Cottage", "Valley Stream", "Verbank", "Verona",
-  "Verplanck", "Vestal", "Victor", "Victory Mills", "Voorheesville",
-  "Waccabuc", "Wading River", "Wainscott", "Walden", "Wallkill",
-  "Walton", "Walworth", "Wantagh", "Wappingers Falls", "Warners",
-  "Warwick", "Washingtonville", "Water Mill", "Waterport", "Watertown",
-  "Watervliet", "Waverly", "Webster", "Weedsport", "West Babylon",
-  "West Chazy", "West Coxsackie", "West Edmeston", "West Falls", "West Harrison",
-  "West Haverstraw", "West Hempstead", "West Henrietta", "West Hurley", "West Islip",
-  "West Nyack", "West Point", "West Sand Lake", "West Sayville", "West Shokan",
-  "Westbrookville", "Westbury", "Westerlo", "Westfield", "Westhampton Beach",
+  "Westtown", "White Plains", "Whitesboro", "Whitestone", "Whitney Point",
+  "Williamson", "Williston Park", "Wingdale", "Winthrop", "Woodbury",
+  "Woodhaven", "Woodmere", "Woodside", "Woodstock", "Wurtsboro",
+  "Wyandanch", "Wynantskill", "Yaphank", "Yonkers", "Yorktown Heights",
 ];
 
 const BATCH_SIZE = 5;
